90c4ae42f9a3a81b3d0375c41a12112a127d0be5
Anton Yurchenko <webbymasta@gmail.com>
2021-12-31 12:45:07 +0200

    Animation workshop

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,101 +1,151 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES, WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { keyframes } from "styled-components";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  return (
-    <Overlay isOpen={isOpen} onDismiss={onDismiss}>
-      <Content aria-label="Menu">
-        <CloseButton onClick={onDismiss}>
-          <Icon id="close" />
-          <VisuallyHidden>Dismiss menu</VisuallyHidden>
-        </CloseButton>
-        <Filler />
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Footer>
-          <SubLink href="/terms">Terms and Conditions</SubLink>
-          <SubLink href="/privacy">Privacy Policy</SubLink>
-          <SubLink href="/contact">Contact Us</SubLink>
-        </Footer>
-      </Content>
-    </Overlay>
-  );
+    return (
+        <Overlay isOpen={isOpen} onDismiss={onDismiss}>
+            <Content aria-label="Menu">
+                <CloseButton onClick={onDismiss}>
+                    <Icon id="close" />
+                    <VisuallyHidden>Dismiss menu</VisuallyHidden>
+                </CloseButton>
+                <Filler />
+                <Nav>
+                    <NavLink href="/sale">Sale</NavLink>
+                    <NavLink href="/new">New&nbsp;Releases</NavLink>
+                    <NavLink href="/men">Men</NavLink>
+                    <NavLink href="/women">Women</NavLink>
+                    <NavLink href="/kids">Kids</NavLink>
+                    <NavLink href="/collections">Collections</NavLink>
+                </Nav>
+                <Footer>
+                    <SubLink href="/terms">Terms and Conditions</SubLink>
+                    <SubLink href="/privacy">Privacy Policy</SubLink>
+                    <SubLink href="/contact">Contact Us</SubLink>
+                </Footer>
+            </Content>
+        </Overlay>
+    );
 };
 
-const Overlay = styled(DialogOverlay)`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: var(--color-backdrop);
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const Content = styled(DialogContent)`
-  background: white;
-  width: 300px;
-  height: 100%;
-  padding: 24px 32px;
-  display: flex;
-  flex-direction: column;
-`;
-
-const CloseButton = styled(UnstyledButton)`
-  position: absolute;
-  top: 10px;
-  right: 0;
-  padding: 16px;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
-const NavLink = styled.a`
-  color: var(--color-gray-900);
-  font-weight: ${WEIGHTS.medium};
-  text-decoration: none;
-  font-size: 1.125rem;
-  text-transform: uppercase;
-
-  &:first-of-type {
-    color: var(--color-secondary);
+const opacityAnimation = keyframes`
+  from {
+    background: var(--color-backdrop-opacity);
+  }
+  to {
+    background: var(--color-backdrop);
   }
 `;
 
+const contentAnimation = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+
+const fadeInAnimation = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const Overlay = styled(DialogOverlay)`
+    --fade-in-animation-delay: 250ms;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: var(--color-backdrop);
+    display: flex;
+    justify-content: flex-end;
+
+    animation: ${opacityAnimation} 400ms;
+`;
+
+const Content = styled(DialogContent)`
+    background: white;
+    width: 300px;
+    height: 100%;
+    padding: 24px 32px;
+    display: flex;
+    flex-direction: column;
+
+    @media (preferce-reduced-motion: no-preference) {
+        animation: ${contentAnimation} 300ms linear;
+    }
+`;
+
+const CloseButton = styled(UnstyledButton)`
+    position: absolute;
+    top: 10px;
+    right: 0;
+    padding: 16px;
+
+    @media (preferce-reduced-motion: no-preference) {
+        animation: ${fadeInAnimation} 300ms both;
+        animation-delay: var(--fade-in-animation-delay);
+    }
+`;
+
+const Nav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    @media (preferce-reduced-motion: no-preference) {
+        animation: ${fadeInAnimation} 300ms both;
+        animation-delay: var(--fade-in-animation-delay);
+    }
+`;
+
+const NavLink = styled.a`
+    color: var(--color-gray-900);
+    font-weight: ${WEIGHTS.medium};
+    text-decoration: none;
+    font-size: 1.125rem;
+    text-transform: uppercase;
+
+    &:first-of-type {
+        color: var(--color-secondary);
+    }
+`;
+
 const Filler = styled.div`
-  flex: 1;
+    flex: 1;
 `;
 const Footer = styled.footer`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  gap: 14px;
-  justify-content: flex-end;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    justify-content: flex-end;
+
+    @media (preferce-reduced-motion: no-preference) {
+        animation: ${fadeInAnimation} 300ms both;
+        animation-delay: var(--fade-in-animation-delay);
+    }
 `;
 
 const SubLink = styled.a`
-  color: var(--color-gray-700);
-  font-size: 0.875rem;
-  text-decoration: none;
+    color: var(--color-gray-700);
+    font-size: 0.875rem;
+    text-decoration: none;
 `;
 
 export default MobileMenu;
